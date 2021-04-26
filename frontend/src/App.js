52ca7ca814b54ac72718bab9b87714eb1cf355d7
Deutsch Gábor <deutschgabor92@gmail.com>
2021-04-26 15:32:34 +0200

    Header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../src/pages/Login/Login';
+import Header from './parts/Header/Header';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/login">
             <Login />
