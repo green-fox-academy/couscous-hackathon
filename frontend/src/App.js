57cfd7d72a1baae0134aaf7219ccd9407f3f9cb0
Deutsch Gábor <deutschgabor92@gmail.com>
2021-04-26 15:03:54 +0200

    Register page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../src/pages/Login/Login';
 import Header from './parts/Header/Header';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Switch>
           <Route exact path="/login">
             <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
           </Route>
         </Switch>
       </div>
