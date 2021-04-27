7a299e729ea804af65f4b9628bda03168d338aa9
Pal_Dorka <pal.dorka.91@gmail.com>
2021-04-27 10:48:18 +0200

    Remove folders build .gradle .idea

--- backend/build/resources/main/db/migration/V1__create_tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user
-(
-    id       bigint       not null auto_increment,
-    username varchar(255) not null,
-    email    varchar(255) not null,
-    password varchar(255),
-    primary key (id)
-);
