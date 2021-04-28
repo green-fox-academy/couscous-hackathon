e9de7ff31ac1674c1f6b864c007f4ba0176281a2
Phan Xuan Phuong <pxphuong94@gmail.com>
2021-04-28 15:39:55 +0200

    BE-04 - GET /cart

---
+++ backend/src/main/java/com/greenfoxacademy/webshop/exception/CartNotFoundException.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.webshop.exception;
+
+public class CartNotFoundException extends CartException{
+  public CartNotFoundException(String message) {
+    super(message);
+  }
+}
---
+++ backend/src/main/java/com/greenfoxacademy/webshop/model/CartResponseDTO.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.webshop.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartResponseDTO {
+  List<CartItemResponseDTO> cartItemResponseDTOList;
+  @JsonProperty(value = "final_price")
+  Integer finalPrice;
+}
