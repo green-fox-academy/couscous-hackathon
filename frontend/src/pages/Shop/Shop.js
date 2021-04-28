b19f7a5138b2415fa3ea6b6371ae960ff52a16b8
Eva Szentmiklosi <eszent@gmail.com>
2021-04-28 22:36:54 +0200

    added name and price filter

--- frontend/src/pages/Shop/Shop.js
+++ frontend/src/pages/Shop/Shop.js
@@ -9,9 +9,8 @@ import { Pagination } from 'antd';
 import 'antd/dist/antd.css';
 
 const { Panel } = Collapse;
-//const backendUrl = process.env.REACT_APP_API_URL;
-//const url = `${backendUrl}/item`;
-//const url2 = `https://fakestoreapi.com/products`;
+const backendUrl = process.env.REACT_APP_API_URL;
+
 
 const Shop = () => {
   const reducerItemState = useSelector((state) => state.itemState);
@@ -25,13 +24,20 @@ const Shop = () => {
   const [pageSize, setPageSize] = useState(6);
   const onShowSizeChange = (current, size) => {
     setPageSize(size);
-  };
+  }
+  const [name, setName] = useState('');
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(1000);
 
-  useEffect(() => {
+  
+  //document.addEventListener('readystatechange', (event) => {
+  //  ShowAll(event)
+  //});
+  
+    useEffect(() => {
+    const url = `${backendUrl}/item?page=${page-1}&pageSize=${pageSize}`
     try {
-      const response = fetch(
-        `http://localhost:8080/item?page=${page - 1}&pageSize=${pageSize}`
-      )
+      const response = fetch(url)
         .then((response) => response.json())
         .then((response) => dispatch(saveAllItems(response)));
       if (response.status !== 200) {
@@ -42,61 +48,88 @@ const Shop = () => {
       setError(error.message);
     }
   }, [page, pageSize, dispatch]);
-
+   
+  
   function callback(key) {
-    console.log(key);
+  console.log(key);
   }
 
+  const onClick = async (event) => {
+    setError(null);
+    event.preventDefault();
+    const searchUrl = `${backendUrl}/item?page=${page - 1}&pageSize=${pageSize}&search=title:${name}&search=price<${max}&search=price>${min}`
+    try {
+      const response = await fetch(searchUrl)
+        if (response.status !== 200) {
+          throw Error(response.error);
+        }
+      const responseObject = await response.json();
+      dispatch(saveAllItems(responseObject));
+      if (responseObject.length !== 0) {
+        return responseObject
+      }
+      setError('We are sorry we have no items with these criterias!')
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  }
+  const showAll = () => {
+    setName('');
+    setMin(0);
+    setMax(1000);
+  }
   return (
     <div className="store-wrapper">
       <div className="searchbar">
-        <Collapse defaultActiveKey={['1']} onChange={callback}>
-          <Panel header=" Filter by name" key="1">
-            <form className="form">
-              <label for="name">Name</label>
+        <form className="form">
+          
+          <Collapse defaultActiveKey={['1']} onChange={callback}>
+            <Panel header=" Filter by name" key="1">
+              <label for="name"></label>
               <input
                 type="text"
                 id="name"
                 name="name"
                 autoComplete="off"
-              ></input>
-            </form>
-          </Panel>
-          <Panel header=" Filter by price" key="2">
-            <form className="form">
-              <label for="price">Price min</label>
-              <input
-                type="number"
-                id="price"
-                name="price"
-                autoComplete="off"
-              ></input>
-            </form>
-          </Panel>
-          <Panel header=" Filter by description" key="3">
-            <form className="form">
-              <label for="category">Category</label>
-              <input
-                type="text"
-                id="category"
-                name="category"
-                autoComplete="off"
-              ></input>
-            </form>
-          </Panel>
-        </Collapse>
-        <button>Search</button>
+                onChange={ (e) => {
+                  setName(e.target.value);
+                } }>
+              </input>
+            </Panel>
+            <Panel header=" Filter by price" key="2">
+              <label for="min">min   </label>
+              <input type="number" id="min" name="min" autoComplete="off"
+                onChange={ (e) => {
+                  setMin(e.target.value);
+                } }>
+              </input>
+          
+              <label for="max">max</label>
+              <input type="number" id="max" name="max" autoComplete="off"
+              onChange={ (e) => {
+                  setMax(e.target.value);
+                } }>
+              </input>
+            </Panel>
+            <Panel header=" Filter by description" key="3">
+              <label for="category">Funny  </label>
+              <input type="radio" id="category" name="category" autoComplete="off"></input>
+              <label for="category">Dramatic  </label>
+              <input type="radio" id="category" name="category" autoComplete="off"></input>
+              <label for="category">Fairy  </label>
+              <input type="radio" id="category" name="category" autoComplete="off"></input>
+              <label for="category"><span><span></span></span>Surreal</label>
+              <input type="radio" id="category" name="category" autoComplete="off"></input>
+            </Panel>
+          </Collapse>
+          <button onClick={ onClick }>Search</button>
+          <button onClick={ showAll}>Back to all</button>
+        </form>
       </div>
       <div className="store-container">
+          
         <div className="store">
-          <div className="welcome">
-            <h2>
-              Welcome! If you are looking for a good cause, please consider
-              helping us to rehabilitate and foster our foxes for release back
-              into the wild. Your purchase will provide basic medicine for
-              lifesaving treatment - and you will have a cute fox buddy too!
-            </h2>
-          </div>
           {error && <div>{error}</div>}
           {reducerItemState.items &&
             reducerItemState.items.map((item) => (
