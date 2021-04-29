346cfbb23a3795d26ea5e54f5f29b4a53c7ef8c1
Pal_Dorka <pal.dorka.91@gmail.com>
2021-04-29 16:08:04 +0200

    checkout email

--- backend/src/main/java/com/greenfoxacademy/webshop/controller/ItemController.java
+++ backend/src/main/java/com/greenfoxacademy/webshop/controller/ItemController.java
@@ -4,6 +4,10 @@ import com.greenfoxacademy.webshop.exception.CartNotFoundException;
 import com.greenfoxacademy.webshop.exception.ItemNotFoundException;
 import com.greenfoxacademy.webshop.model.ItemDescriptionDTO;
 import com.greenfoxacademy.webshop.model.ItemResponseDTO;
+import com.greenfoxacademy.webshop.repository.PurchaseRepository;
+import com.greenfoxacademy.webshop.repository.UserRepository;
+import com.greenfoxacademy.webshop.service.CartService;
+import com.greenfoxacademy.webshop.service.EmailService;
 import com.greenfoxacademy.webshop.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -21,6 +26,9 @@ public class ItemController {
 
     @Autowired
     private ItemService itemService;
+
+    @Autowired
+    private CartService cartService;
 
     @CrossOrigin
     @GetMapping("/item/{id}")
@@ -35,11 +43,21 @@ public class ItemController {
     public ResponseEntity<List<ItemResponseDTO>> getItemList(
             @RequestParam(required = false) String search,
             @RequestParam Integer page,
-            @RequestParam Integer pageSize
-    ) {
+            @RequestParam Integer pageSize,
+            HttpServletRequest request) {
 
         return ResponseEntity.ok(
-                itemService.getItems(search, page, pageSize)
-        );
+                itemService.getItems(search, page, pageSize, request));
+    }
+
+    @Autowired
+    EmailService emailService;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PurchaseRepository purchaseRepository;
+    @GetMapping("/test")
+    public void test() throws MessagingException {
+        emailService.sendCheckoutMessage(userRepository.findById(9L).get(), purchaseRepository.findById(1L).get());
     }
 }
--- backend/src/main/java/com/greenfoxacademy/webshop/service/EmailService.java
+++ backend/src/main/java/com/greenfoxacademy/webshop/service/EmailService.java
@@ -1,42 +1,69 @@
 package com.greenfoxacademy.webshop.service;
 
+import com.greenfoxacademy.webshop.model.Purchase;
+import com.greenfoxacademy.webshop.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
 
 @Component
 public class EmailService {
 
     private JavaMailSender emailSender;
+    private SpringTemplateEngine templateEngine;
 
     @Autowired
-    public EmailService(@Qualifier("getJavaMailSender") JavaMailSender emailSender) {
+    public EmailService(
+            @Qualifier("getJavaMailSender") JavaMailSender emailSender,
+            SpringTemplateEngine templateEngine) {
         this.emailSender = emailSender;
+        this.templateEngine = templateEngine;
     }
 
     public void sendRegistrationMessage(String to, String url, String token)
             throws MessagingException {
+
+        String subject = "Registration Confirmation";
+        String htmlBody = "<html><body>" +
+                "<p>Thank you for your registration!<p>" +
+                "<p>Please confirm it by clicking on the following link: >" +
+                "<a href=\"" + url + "/registration-confirm?token=" + token + "\">" + token + "</a>" +
+                "<br><p>Couscous Fox Hospital<p>" +
+                "</body></html>";
+
+        sendHtmlMessage(to, subject, htmlBody);
+    }
+
+    public void sendCheckoutMessage(User user, Purchase purchase) throws MessagingException {
+        HashMap<String, Object> templateModel = new HashMap<>();
+        templateModel.put("username", user.getUsername());
+        templateModel.put("purchaseAmounts", purchase.getPurchaseAmounts());
+
+        Context checkoutContext = new Context();
+        checkoutContext.setVariables(templateModel);
+        String htmlBody = templateEngine.process("checkout-template.html", checkoutContext);
+
+        sendHtmlMessage(user.getEmail(), "Couscous Fox Hospital contribution", htmlBody);
+    }
+
+    private void sendHtmlMessage(String to, String subject, String htmlBody) throws MessagingException {
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         helper.setFrom("couscousfoxhospital.noreply@gmail.com");
         helper.setTo(to);
-        helper.setSubject("Registration Confirmation");
-        helper.setText("<html><body>" +
-                "<p>Thank you for your registration!<p><br>" +
-                "<p>Please confirm it by clicking on the following link:>" +
-                "<a href=\"" + url + "registration-confirm?token=" + token + "\">" + token + "</a>" +
-                "<br><p>Couscous Fox Hospital<p>" +
-                "</body></html>", true);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
 
         emailSender.send(message);
     }
-
-
 
 }
